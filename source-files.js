7af47fdc481e829f5b35cf5c319aa94d8bd22386
fu5ha <fu5ha@users.noreply.github.com>
2022-04-25 19:25:28 +0000

    deploy: 94e368bebe6dd79c2cedda7a225142e4671f2fc6

---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bytemuck"] = {"name":"","files":["anybitpattern.rs","checked.rs","contiguous.rs","internal.rs","lib.rs","no_uninit.rs","offset_of.rs","pod.rs","transparent.rs","zeroable.rs"]};
+sourcesIndex["cint"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
